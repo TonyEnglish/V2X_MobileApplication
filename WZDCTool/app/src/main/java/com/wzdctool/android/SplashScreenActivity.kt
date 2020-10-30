8a675b43882473befeedd03663750b7dfdbd0cb8
Drew Johnston <31270488+drewjj@users.noreply.github.com>
2020-10-30 16:57:44 -0600

    Splash screen

---
+++ WZDCTool/app/src/main/java/com/wzdctool/android/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.wzdctool.android
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        Handler().postDelayed(Runnable {
+            // Start your app main activity
+            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(i)
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
